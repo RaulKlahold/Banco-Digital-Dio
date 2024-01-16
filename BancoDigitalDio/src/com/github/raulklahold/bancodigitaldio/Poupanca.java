7a8746f5259b75f272d47884a8477500fe508b2c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-15 21:53:03 -0300

    att poupanca

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/Poupanca.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/Poupanca.java
@@ -1,5 +1,13 @@
 package com.github.raulklahold.bancodigitaldio;
 
 public class Poupanca {
-
+	 public static void Contacorrente(ColetarDados ColetarDados) {
+  	   String nome = ColetarDados.getNome();
+  	   String agencia = ColetarDados.getAgencia();
+  	   String numeroconta = ColetarDados.getNumeroconta();
+  	   double saldo = ColetarDados.getSaldo();
+  	   
+  	   
+  
+     }
 }
