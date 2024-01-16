ea14c365f607d65aaf676be175cf67da36a8819a
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-15 22:22:42 -0300

    ediatando a main

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/RetornaMain.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/RetornaMain.java
@@ -1,10 +1,19 @@
 package com.github.raulklahold.bancodigitaldio;
 
+import java.util.Scanner;
+
 public class RetornaMain {
 
 	public static void main(String[] args) {
+		ColetarDados cx = new ColetarDados();
+		Scanner scan = new Scanner(System.in);
 		
+		
+	System.out.println("\nBem vindos ao Banco Digital! \n");
+	System.out.println("\nPara qual opção você deseja ter acesso? \n");
+	
 
 	}
+
 
 }
