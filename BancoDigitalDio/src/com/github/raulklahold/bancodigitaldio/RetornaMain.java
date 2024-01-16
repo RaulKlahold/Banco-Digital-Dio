0751530b581be9e441eb06e05bc18139f0a3c735
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-15 22:42:34 -0300

    att a main

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/RetornaMain.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/RetornaMain.java
@@ -7,25 +7,25 @@ public class RetornaMain {
 	public static void main(String[] args) {
 		ColetarDados cx = new ColetarDados();
 		Scanner scan = new Scanner(System.in);
-		
-		
-	System.out.println("\nBem vindos ao Banco Digital! \n");
-	System.out.println("\nPara qual opção você deseja ter acesso? \n");
-	System.out.println("Escolha a seu tipo de conta C - conta corrente P - poupança: \n");
-	String opcao = scan.nextLine();
-	
-	if(opcao == "C" || opcao == "c") {
-		
-		
-	}
-	else if(opcao == "p" || opcao == "P") {
-		
-	}else {
-		System.out.println("Colocar uma exception aqui");
-	}
-	
 
+		System.out.println("\nBem vindos ao Banco Digital! \n");
+		System.out.println("\nPara qual opção você deseja ter acesso? \n");
+		System.out.println("Escolha a seu tipo de conta 1 - conta corrente 2 - poupança: \n");
+		int opcao = scan.nextInt();
+
+		switch (opcao) {
+		case 1:
+			System.out.println("opçao C maiusculo");
+			break;
+
+		case 2:
+			System.out.println("opçao P minusculo");
+			break;
+
+		default:
+			System.out.println("Pedido não encontrado");
+			break;
+
+		}
 	}
-
-
 }
