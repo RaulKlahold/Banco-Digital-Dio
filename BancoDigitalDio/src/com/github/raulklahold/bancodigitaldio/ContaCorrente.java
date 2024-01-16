f917daa39dbac8bd9b42c7cfce5e68ec4af142a2
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-15 21:51:49 -0300

    criando conta corrente class

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
@@ -1,7 +1,13 @@
 package com.github.raulklahold.bancodigitaldio;
 
 public class ContaCorrente {
-       public static String Contacorrente(ColetarDados ColetarDados) {
+       public static void Contacorrente(ColetarDados ColetarDados) {
+    	   String nome = ColetarDados.getNome();
+    	   String agencia = ColetarDados.getAgencia();
+    	   String numeroconta = ColetarDados.getNumeroconta();
+    	   double saldo = ColetarDados.getSaldo();
     	   
+    	   
+    
        }
 }
