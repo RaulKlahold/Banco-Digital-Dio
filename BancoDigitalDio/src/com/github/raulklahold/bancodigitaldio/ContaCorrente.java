7c311cde220371f21c93530313c2b23dfd6204b8
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-17 17:55:54 -0300

    arrumando pequenos erros

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
@@ -3,7 +3,7 @@ package com.github.raulklahold.bancodigitaldio;
 public class ContaCorrente {
        public static String Contacorrente(ColetarDados ColetarDados) {
     	   String nome = ColetarDados.getNome();
-    	   String agencia = ColetarDados.getAgencia();
+    	   int agencia = ColetarDados.getAgencia();
     	   String numeroconta = ColetarDados.getNumeroconta();
     	   double saldo = ColetarDados.getSaldo();
     	   
