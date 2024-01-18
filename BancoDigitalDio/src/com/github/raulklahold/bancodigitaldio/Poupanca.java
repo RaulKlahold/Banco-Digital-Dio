8c3741d5391f098137995c3fbc8c6c291b18ca8c
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-18 19:09:28 -0300

    finalizando as exceptions

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/Poupanca.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/Poupanca.java
@@ -1,13 +1,22 @@
 package com.github.raulklahold.bancodigitaldio;
 
 public class Poupanca {
-	 public static String Contacorrente(ColetarDados ColetarDados) {
-		
+	 public static String Contacorrente(ColetarDados ColetarDados) throws ExceptionConta, ExceptionNome {
+
+
 		    	   String nome = ColetarDados.getNome();
 		    	   int agencia = ColetarDados.getAgencia();
 		    	   String numeroconta = ColetarDados.getNumeroconta();
 		    	   double saldo = ColetarDados.getSaldo();
-		    	   
+
+		   		if (nome.length() < 3) {
+		   			throw new ExceptionNome();
+		   		}
+
+		   		else if (numeroconta.length() < 7) {
+		   			throw new ExceptionConta();
+		   		} else {
+
 		    	   String credencial = "Olá, " + nome + " Tudo bem?\n" +
 		                               "Sua agência é: " + agencia + "\n" +
 		                               "Sua conta é: " + numeroconta + "\n" +
@@ -19,5 +28,5 @@ public class Poupanca {
 		       }
   	  
   
-     }
+     }}
 
