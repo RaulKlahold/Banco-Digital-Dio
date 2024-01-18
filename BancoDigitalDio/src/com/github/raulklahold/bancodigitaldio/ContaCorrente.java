1ee31156aa7cadaeaebc85603c54f6f390ccdf3b
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-18 19:08:10 -0300

    arrumando as exceptions

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ContaCorrente.java
@@ -1,30 +1,26 @@
 package com.github.raulklahold.bancodigitaldio;
 
 public class ContaCorrente {
-       public static String Contacorrente(ColetarDados ColetarDados) {
-    	   String nome = ColetarDados.getNome();
-    	   int agencia = ColetarDados.getAgencia();
-    	   String numeroconta = ColetarDados.getNumeroconta();
-    	   double saldo = ColetarDados.getSaldo();
-    	   if(nome.length() < 3) {
-   			throw new ExceptionNome();
-   		}
-   		else if(agencia.length() < 4 && agencia.length() > 5) {
-   			throw new ExceptionAgencia();
-   			
-   		}
-   		else if(numeroconta.length() < 7) {
-   			throw new ExceptionPedido();
-   		}
-   		else {
+	public static String Contacorrente(ColetarDados ColetarDados) throws ExceptionConta, ExceptionNome {
+		String nome = ColetarDados.getNome();
+		int agencia = ColetarDados.getAgencia();
+		String numeroconta = ColetarDados.getNumeroconta();
+		double saldo = ColetarDados.getSaldo();
 
-    	   String credencial = "Olá, " + nome + " Tudo bem?\n" +
-                               "Sua agência é: " + agencia + "\n" +
-                               "Sua conta é: " + numeroconta + "\n" +
-                               "E seu saldo é: " + saldo;
-    	   
-    	   System.out.println(credencial);
-    	   
-    	   return credencial;
-       }
-}}
+		if (nome.length() < 3) {
+			throw new ExceptionNome();
+		}
+
+		else if (numeroconta.length() < 7) {
+			throw new ExceptionConta();
+		} else {
+
+			String credencial = "Olá, " + nome + " Tudo bem?\n" + "Sua agência é: " + agencia + "\n" + "Sua conta é: "
+					+ numeroconta + "\n" + "E seu saldo é: " + saldo;
+
+			System.out.println(credencial);
+
+			return credencial;
+		}
+	}
+}
