b7390b6b09847e390c09fda831f8c8ee6b51160e
Raul Klahold <126357426+RaulKlahold@users.noreply.github.com>
2024-01-18 19:02:42 -0300

    adicionando exceptions agenccia

--- BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ExceptionAgencia.java
+++ BancoDigitalDio/src/com/github/raulklahold/bancodigitaldio/ExceptionAgencia.java
@@ -1,5 +1,14 @@
 package com.github.raulklahold.bancodigitaldio;
 
-public class ExceptionAgencia {
+@SuppressWarnings("serial")
 
+public class ExceptionAgencia extends Exception {
+	public ExceptionAgencia() {
+		super("O nome precisa ter no minimo 3 caracteres: ");
+	}
+
+	public ExceptionAgencia(String message) {
+		super(message);
+	}
 }
+
